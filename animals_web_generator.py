9913fd7023a0d3320899a3c102364001b1fc1829
MaxiMatrix <ms6schmidtma@hotmail.de>
2024-09-29 22:07:33 +0200

    added the serialize_animal function

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,38 +1,46 @@
 import json
 
+
 def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+    """Loads a JSON file"""
+    with open(file_path, "r", encoding="utf-8") as handle:  # Ensure UTF-8 encoding for JSON file
+        return json.load(handle)
 
 
+# Read the HTML template with UTF-8 encoding
 with open("animals_template.html", "r", encoding="utf-8") as file:
     html_content = file.read()
 
 
+# Load animals data from JSON file
 animals_data = load_data('animals_data.json')
 
-output = "" # define an empty string
-for animal in animals_data:
-    output += f'<li class="cards__item">'
-    output += f'<div class="card__title">{animal["name"]}</div>\n'
-    output += f'<p class="card__text">'
-    output += f'<strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
-    output += f'<strong>Location:</strong> {animal["locations"][0]}<br/>\n'
+
+def serialize_animal(animal_obj):
+    """Serializes an animal object into an HTML list item"""
+    output = f'<li class="cards__item">\n'
+    output += f'  <div class="card__title">{animal_obj["name"]}</div>\n'
+    output += f'  <p class="card__text">\n'
+    output += f'    <strong>Diet:</strong> {animal_obj["characteristics"]["diet"]}<br/>\n'
+    output += f'    <strong>Location:</strong> {animal_obj["locations"][0]}<br/>\n'
     try:
-        output += f'<strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n'
-        output += f'</p>\n'
-        output += f'</li>\n'
+        output += f'    <strong>Type:</strong> {animal_obj["characteristics"]["type"]}<br/>\n'
     except KeyError:
-        output += f'</p>\n'
-        output += f'</li>\n'
+        pass  # Handle case where 'type' is missing
+    output += f'  </p>\n'
+    output += f'</li>\n'
+    return output
 
 
+# Serialize all animals into HTML
+output = ''.join([serialize_animal(animal_obj) for animal_obj in animals_data])
+
+
+# Replace the placeholder in the HTML template
 filler_txt = "__REPLACE_ANIMALS_INFO__"
 website_txt = html_content.replace(filler_txt, output)
 
 
+# Write the final HTML output to a file with UTF-8 encoding
 with open("animals.html", "w", encoding="utf-8") as file:
     file.write(website_txt)
-
-print(output)
