f34a48af3d86fcc2e6881932a76fada3bb1cc296
MaxiMatrix <ms6schmidtma@hotmail.de>
2024-09-29 21:50:39 +0200

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -10,23 +10,29 @@ with open("animals_template.html", "r", encoding="utf-8") as file:
     html_content = file.read()
 
 
-print(html_content)
 animals_data = load_data('animals_data.json')
 
 output = "" # define an empty string
 for animal in animals_data:
-    output += '<li class="cards__item">'
-    output += f'Name: {animal["name"]}<br/>\n'
-    output += f'Diet: {animal["characteristics"]["diet"]}<br/>\n'
-    output += f'Location: {animal["locations"][0]}<br/>\n'
+    output += f'<li class="cards__item">'
+    output += f'<div class="card__title">{animal["name"]}</div>\n'
+    output += f'<p class="card__text">'
+    output += f'<strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
+    output += f'<strong>Location:</strong> {animal["locations"][0]}<br/>\n'
     try:
-        output += f'Type: {animal["characteristics"]["type"]}<br/>\n'
-        output += '<li/><br/>\n'
+        output += f'<strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n'
+        output += f'</p>\n'
+        output += f'</li>\n'
     except KeyError:
-        output += f'<li/><br/>\n'
+        output += f'</p>\n'
+        output += f'</li>\n'
+
 
 filler_txt = "__REPLACE_ANIMALS_INFO__"
 website_txt = html_content.replace(filler_txt, output)
 
+
 with open("animals.html", "w", encoding="utf-8") as file:
     file.write(website_txt)
+
+print(output)
