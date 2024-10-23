61d01192222815f36f1899b8d45397b93ed9600a
MaxiMatrix <ms6schmidtma@hotmail.de>
2024-10-23 10:37:42 +0200

    added the main function

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,15 +7,6 @@ def load_data(file_path):
         return json.load(handle)
 
 
-# Read the HTML template with UTF-8 encoding
-with open("animals_template.html", "r", encoding="utf-8") as file:
-    html_content = file.read()
-
-
-# Load animals data from JSON file
-animals_data = load_data('animals_data.json')
-
-
 def serialize_animal(animal_obj):
     """Serializes an animal object into an HTML list item"""
     output = f'<li class="cards__item">\n'
@@ -23,24 +14,36 @@ def serialize_animal(animal_obj):
     output += f'  <p class="card__text">\n'
     output += f'    <strong>Diet:</strong> {animal_obj["characteristics"]["diet"]}<br/>\n'
     output += f'    <strong>Location:</strong> {animal_obj["locations"][0]}<br/>\n'
+
     try:
         output += f'    <strong>Type:</strong> {animal_obj["characteristics"]["type"]}<br/>\n'
     except KeyError:
         pass  # Handle case where 'type' is missing
+
     output += f'  </p>\n'
     output += f'</li>\n'
     return output
 
 
-# Serialize all animals into HTML
-output = ''.join([serialize_animal(animal_obj) for animal_obj in animals_data])
+def main():
+    # Read the HTML template with UTF-8 encoding
+    with open("animals_template.html", "r", encoding="utf-8") as file:
+        html_content = file.read()
+
+    # Load animals data from JSON file
+    animals_data = load_data('animals_data.json')
+
+    # Serialize all animals into HTML
+    output = ''.join([serialize_animal(animal_obj) for animal_obj in animals_data])
+
+    # Replace the placeholder in the HTML template
+    filler_txt = "__REPLACE_ANIMALS_INFO__"
+    website_txt = html_content.replace(filler_txt, output)
+
+    # Write the final HTML output to a file with UTF-8 encoding
+    with open("animals.html", "w", encoding="utf-8") as file:
+        file.write(website_txt)
 
 
-# Replace the placeholder in the HTML template
-filler_txt = "__REPLACE_ANIMALS_INFO__"
-website_txt = html_content.replace(filler_txt, output)
-
-
-# Write the final HTML output to a file with UTF-8 encoding
-with open("animals.html", "w", encoding="utf-8") as file:
-    file.write(website_txt)
+if __name__ == "__main__":
+    main()
